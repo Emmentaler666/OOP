596d5128907914431106cdd79cbfbae46a2f4c48
Emmentaler666 <kristijanbobanovic69@gmail.com>
2026-04-13 18:17:24 +0200

    Add files via upload

---
+++ t5/src/zadaci_abs/FizickiProizvod.java
@@ -0,0 +1,25 @@
+package zadaci_abs;
+
+public class FizickiProizvod extends Proizvod{
+
+
+    private static final float TROSAK_DOSTAVA = 0.033f;
+
+    protected FizickiProizvod(String naziv, float jedCijena, float kolicina) {
+        super(naziv, jedCijena, kolicina);
+
+
+    }
+
+    @Override
+    public float ukupnaCijenaProizvoda(){
+        this.ukupnaCijena = this.jedCijena*this.kolicina + this.kolicina*TROSAK_DOSTAVA;
+
+        System.out.println("Ukupna cijena za: "+ this.getClass().getSimpleName() + " je: "+ this.ukupnaCijena);
+        return this.ukupnaCijena;
+    }
+
+
+}
+
+
---
+++ t5/src/zadaci_abs/Test.java
@@ -0,0 +1,13 @@
+package zadaci_abs;
+
+public class Test {
+    public static void main(String[] args) {
+        String[] stringovi = new String[5];
+        System.out.println(stringovi.length);
+        for (String str : stringovi){
+            System.out.println(str);
+        }
+
+        System.out.println(stringovi.length);
+    }
+}
